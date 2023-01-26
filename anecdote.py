3e869e9d9e2eaae40c71cf1dd6f8295dd0909646
Kamol <101630621+kamolgks@users.noreply.github.com>
2023-01-26 17:03:09 +0500

    Mini update 🪄

--- anecdote.py
+++ anecdote.py
@@ -5,13 +5,16 @@ __version__ = (0, 0, 2)
 #  `\__ \ |  _ `\| || |     /' _ ` _ `\ /'_`\  /'_` |( ) ( ) | |  /'__`\/',__)
 #  ( )_) || | | || || |_    | ( ) ( ) |( (_) )( (_| || (_) | | | (  ___/\__, \
 #  `\____)(_) (_)(_)`\__)   (_) (_) (_)`\___/'`\__,_)`\___/'(___)`\____)(____/
-#        
-#            © Copyright 2022
+#
+#              © Copyright 2022
 #
 #          https://t.me/shitmodules
 #
 # 🔒 Licensed under the GNU GPLv3
 # 🌐 https://www.gnu.org/licenses/agpl-3.0.html
+
+# scope: hikka_only
+# scope: hikka_min 1.5.3
 
 # meta banner: https://x0.at/NqCK.gif
 # meta developer: @shitmodules
@@ -22,15 +25,18 @@ import random
 
 chat = "anertsy"
 
+@loader.tds
 class AnecdoteGenerator(loader.Module):
+    """Anecdote generator by @shitmodules"""
 
     strings = {
         "name": "AnecdoteGenerator",
     }
-    
-    async def client_ready(self):
-    	self.messages = await self.client.get_messages(chat, limit=100)
 
+    async def client_ready(self):
+        self.messages = await self.client.get_messages(chat, limit=100)
+
+    @loader.command(ru_doc="Генерирует анекдоты (они не всегда смешные)")
     async def aneccmd(self, message: Message):
         """Sends an anecdote (not always funny)"""
         wtf = random.choice(self.messages)
