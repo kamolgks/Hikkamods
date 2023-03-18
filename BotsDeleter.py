b35f7af88534bd53f930d48dc2680ed646587431
Kamoliddin Tukhtaboev <101630621+kamolgks@users.noreply.github.com>
2023-03-18 14:18:20 +0500

    Update BotsDeleter.py

--- BotsDeleter.py
+++ BotsDeleter.py
@@ -17,72 +17,68 @@ __version__ = (0, 0, 1)
 # scope: hikka_min 1.6.0
 
 # meta pic: https://raw.githubusercontent.com/kamolgks/assets/main/DelBots.png
-# meta banner: https://x0.at/3u3O.mp4
 # meta developer: @shitmodules
 
 from .. import loader, utils
 
-from telethon import events
 from telethon import functions
 from telethon.tl.types import Message
-from telethon.sync import TelegramClient
 from telethon.tl.functions.channels import JoinChannelRequest
-
 
 @loader.tds
 class BotsDeleterMod(loader.Module):
     """Instant stop or removal of all running Telegram bots"""
 
     strings = {
-      "name": "BotsDeleter",
-      "author": "shitmodules",
-      "processing": "<emoji document_id=5213452215527677338>⏳</emoji><b>Starting to stop bots...</b>",
-      "assist": "<emoji document_id=5213452215527677338>⏳</emoji><b>I'm starting to remove bots...</b>",
-      "stop": "<emoji document_id=5418063924933173277>👨‍💻</emoji><b>All bots have been successfully stopped</b>",
-      "del": "<emoji document_id=5418063924933173277>👨‍💻</emoji><b>All bots have been successfully removed</b>", 
+        "name": "BotsDeleter",
+        "author": "shitmodules",
+        "processing": "<emoji document_id=5213452215527677338>⏳</emoji><b>Starting to stop bots...</b>",
+        "assist": "<emoji document_id=5213452215527677338>⏳</emoji><b>I'm starting to remove bots...</b>",
+        "stop": "<emoji document_id=5418063924933173277>👨‍💻</emoji><b>All bots have been successfully stopped</b>",
+        "del": "<emoji document_id=5418063924933173277>👨‍💻</emoji><b>All bots have been successfully removed</b>", 
     }
 
+    strings_ru = {
+        "processing": "<emoji document_id=5213452215527677338>⏳</emoji><b>Начинаем останавливать ботов...</b>",
+        "assist": "<emoji document_id=5213452215527677338>⏳</emoji><b>Я начинаю удалять ботов...</b>",
+        "stop": "<emoji document_id=5418063924933173277>👨‍💻</emoji><b>Все боты были успешно остановлены</b>",
+        "del": "<emoji document_id=5418063924933173277>👨‍💻</emoji><b>Все боты были успешно удалены</b>",
+    }
 
     async def client_ready(self, client, db):
         self.db = db
         self.client = client
-        shit = (await client.get_messages(self.strings("author"), ids=47))
+        shit = (await client.get_messages(self.strings("author"), ids=46))
         await shit.react("❤️")
 
         await client(JoinChannelRequest(channel=self.strings("author")))
-
+        
+        # КТО ПРОЧИТАЕТ ЭТО ТОТ - ЛОХ
 
     @loader.command(ru_doc="> Чтобы остановить работу всех ботов")
     async def stopallbotscmd(self, message: Message):
         """> To stop all bots from working"""
-
-        await utils.answer(message, self.strings("processing"))
-
+        msg = await utils.answer(message, self.strings("processing"))
         t = self.strings("stop")
         k = ""
-
         async for dialog in self.client.iter_dialogs():
             if hasattr(dialog.entity, "bot"):
                 if dialog.entity.bot == True:
                     k += "@" + dialog.entity.username + "has ID" + str(dialog.id) + "\n"
-                    result = await self.client(functions.contacts.BlockRequest(id=dialog.id))
+                    await self.client(functions.contacts.BlockRequest(id=dialog.id))
                     
-        return await utils.answer(message, f"{t}")
-
+        await msg.edit(f"{t}")
 
     @loader.command(ru_doc="> Чтобы удалить диалоги со всеми ботами")
     async def delallbotscmd(self, message: Message):
         """> To delete dialogs with all bots"""
-
-        await utils.answer(message, self.strings("assist"))
-
+        msg = await utils.answer(message, self.strings("assist"))
         t = self.strings("del")
         k = ""
-
         async for dialog in self.client.iter_dialogs():
             if hasattr(dialog.entity, "bot"):
                 if dialog.entity.bot == True:
                     k += "@" + dialog.entity.username + "has ID" + str(dialog.id) + "\n"
                     await dialog.delete()
 
-        return await utils.answer(message, f"{t}")
+        await msg.edit(f"{t}")
