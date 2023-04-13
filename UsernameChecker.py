9df9d0faab6250558a1e62812f057ebdb5c7ae4e
Kamoliddin Tukhtaboev <101630621+kamolgks@users.noreply.github.com>
2023-04-13 19:19:46 +0500

    Update UsernameChecker.py

--- UsernameChecker.py
+++ UsernameChecker.py
@@ -1,80 +1,87 @@
 __version__ = (0, 0, 1)
-#   ___    _         _                             _         _                
-#  (  _`\ ( )     _ ( )_                          ( )       (_ )              
-#  | (_(_)| |__  (_)| ,_)     ___ ___     _      _| | _   _  | |    __    ___ 
-#  `\__ \ |  _ `\| || |     /' _ ` _ `\ /'_`\  /'_` |( ) ( ) | |  /'__`\/',__)
-#  ( )_) || | | || || |_    | ( ) ( ) |( (_) )( (_| || (_) | | | (  ___/\__, \
-#  `\____)(_) (_)(_)`\__)   (_) (_) (_)`\___/'`\__,_)`\___/'(___)`\____)(____/
-#
-#              © Copyright 2023
-#
-#          https://t.me/shitmodules
-#
-# 🔒 Licensed under the GNU GPLv3
-# 🌐 https://www.gnu.org/licenses/agpl-3.0.html
+# *
+# *              $$\       $$\   $$\                                   $$\           $$\
+# *              $$ |      \__|  $$ |                                  $$ |          $$ |
+# *     $$$$$$$\ $$$$$$$\  $$\ $$$$$$\   $$$$$$\$$$$\   $$$$$$\   $$$$$$$ |$$\   $$\ $$ | $$$$$$\   $$$$$$$\
+# *    $$  _____|$$  __$$\ $$ |\_$$  _|  $$  _$$  _$$\ $$  __$$\ $$  __$$ |$$ |  $$ |$$ |$$  __$$\ $$  _____|
+# *    \$$$$$$\  $$ |  $$ |$$ |  $$ |    $$ / $$ / $$ |$$ /  $$ |$$ /  $$ |$$ |  $$ |$$ |$$$$$$$$ |\$$$$$$\
+# *     \____$$\ $$ |  $$ |$$ |  $$ |$$\ $$ | $$ | $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |$$   ____| \____$$\
+# *    $$$$$$$  |$$ |  $$ |$$ |  \$$$$  |$$ | $$ | $$ |\$$$$$$  |\$$$$$$$ |\$$$$$$  |$$ |\$$$$$$$\ $$$$$$$  |
+# *    \_______/ \__|  \__|\__|   \____/ \__| \__| \__| \______/  \_______| \______/ \__| \_______|\_______/
+# *
+# *
+# *            © Copyright 2023
+# *
+# *         https://t.me/shitmodules
+# *
+# 🔒 Code is licensed under CC-BY-NC-ND 4.0 unless otherwise specified.
+# 🌐 https://creativecommons.org/licenses/by-nc-nd/4.0/
+
+# You CANNOT edit this file without direct permission from the author.
+# You can redistribute this file without any changes.
 
 # scope: hikka_only
-# scope: hikka_min 1.6.0
+# scope: hikka_min 1.6.2
 
-# meta banner: https://0x0.st/Hi9j.gif.mp4
-# meta developer: @shitmodules 
+# meta banner: https://te.legra.ph/file/dfb65efafcbad191f7a46.mp4
+# meta developer: @shitmodules
 
 import logging
 
 from telethon import functions
-from telethon.tl.types import Message # type: ignore
+from hikkatl.types import Message
 
 from .. import loader, utils
 
 logger = logging.getLogger(__name__)
 
+
 @loader.tds
 class UsernameCheckerMod(loader.Module):
-  """
-  A module for checking the user for availability.
-  Accepted characters: A-z (case-insensitive), 0-9 and underscores.
-  Length: 6-32 characters.
-  """
-  
-  strings = {
-    "name": "UsernameChecker",
-    "author": "shitmodules",
-    "true": (
-      "<emoji document_id=5215538598970929961>👌</emoji><i><b>User is free and can be used.</b></i>"
-    ),
-    "wah_args": (
-      "<emoji document_id=5359839982468996640>🦆</emoji>There are no arguments or they are not enough. "
-      "Example of using this module: <code>.ucheck picdez</code> [The user must be no shorter than 6 letters]"
-    ),
-    "false": (
-      "<emoji document_id=5854973145315806460>👮‍♂️</emoji><i><b>The user is already taken by another user, create a new one for yourself.</b></i>"
-    ),
-  }
+    """
+    A module for checking the user for availability.
+    Accepted characters: A-z (case-insensitive), 0-9 and underscores.
+    Length: 6-32 characters.
+    """
 
-  strings_ru = {
-    "true": (
-      "<emoji document_id=5215538598970929961>👌</emoji><i><b>Юзер свободен и может быть использован.</b></i>"
-    ),
-    "wah_args": (
-      "<emoji document_id=5359839982468996640>🦆</emoji>Аргументов нет или их недостаточно. "
-      "Пример использования этого модуля: <code>.ucheck picdez</code> [Имя пользователя должно быть не короче 6 букв]"
-    ),
-    "false": (
-      "<emoji document_id=5854973145315806460>👮‍♂️</emoji><i><b>Юзер уже занят другим пользователем, придумайте себе новый.</b></i>"
-    ),
-  }
+    strings = {
+        "name": "UsernameChecker",
+        "true": (
+            "<emoji document_id=5215538598970929961>👌</emoji><i><b>User is free and can be used.</b></i>"
+        ),
+        "wah_args": (
+            "<emoji document_id=5359839982468996640>🦆</emoji>There are no arguments or they are not enough. "
+            "Example of using this module: <code>.ucheck picdato</code> [The user must be no shorter than 6 letters]"
+        ),
+        "false": (
+            "<emoji document_id=5854973145315806460>👮‍♂️</emoji><i><b>The user is already taken by another user, create a new one for yourself.</b></i>"
+        ),
+    }
 
-  @loader.command(ru_doc="> Введите юзер для проверки.")
-  async def ucheckcmd(self, message: Message):
-    """> Enter the user for verification"""
-    args = utils.get_args_raw(message)
-    result = await message.client(functions.account.CheckUsernameRequest(username=args))
+    strings_ru = {
+        "true": (
+            "<emoji document_id=5215538598970929961>👌</emoji><i><b>Юзер свободен и может быть использован.</b></i>"
+        ),
+        "wah_args": (
+            "<emoji document_id=5359839982468996640>🦆</emoji>Аргументов нет или их недостаточно. "
+            "Пример использования этого модуля: <code>.ucheck pizdato</code> [Имя пользователя должно быть не короче 6 букв]"
+        ),
+        "false": (
+            "<emoji document_id=5854973145315806460>👮‍♂️</emoji><i><b>Юзер уже занят другим пользователем, придумайте себе новый.</b></i>"
+        ),
+    }
 
-    if args == "":
-      return await utils.answer(message, self.strings("wah_args"))
+    @loader.command(ru_doc="> Введите юзер для проверки.")
+    async def ucheckcmd(self, message: Message):
+        """> Enter the user for verification"""
+        args = utils.get_args_raw(message)
+        result = await message.client(functions.account.CheckUsernameRequest(username=args))
 
-    if result == True:
-      return await utils.answer(message, self.strings("true"))
- 
-    if result == False:
-      return await utils.answer(message, self.strings("false"))
+        if args == "":
+            return await utils.answer(message, self.strings("wah_args"))
+
+        if result == True:
+            return await utils.answer(message, self.strings("true"))
+
+        if result == False:
+            return await utils.answer(message, self.strings("false"))
